c4df00f12c0aa4cfcd6eab169ffea74ee57ceda8
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-12-01 19:49:49 -0500

    update greeting

--- commands/greeting.js
+++ commands/greeting.js
@@ -8,16 +8,15 @@ module.exports = {
 	async execute(interaction) {
 		const exampleEmbed = new MessageEmbed()
 			.setColor('#532d8e')
-			.setTitle('Hey there, I\'m Robo-Kathryn!')
-			.setAuthor('Purple Fire Robotics', 'https://i.ibb.co/cDrSdS5/PF-Flame.png', 'https://purplefire.org')
-			.setDescription('I am here to help out with a few of the simple tasks around here. I\'m still learning, so please, have some patience.')
-			.addField('Developer', 'This bot is being developed by <@200316185445793792> and <@581128269584138250>.', true)
-			.addField('Problem?', 'Did I make a mistake? No worries, just tell a Dylan.', true)
-			.addField('Open Source', 'You can find the source code at https://github.com/beachdyl/purple_fire_bot/', false)
-			.addField('Similarity', 'Robo-Kathryn is a work of fiction. Any similarity to actual persons, living or dead, is purely coincidental.', false)
-			.setThumbnail('https://i.ibb.co/cDrSdS5/PF-Flame.png')
+			.setTitle('Hey there, I\'m Sector!')
+			.setDescription('I\'m here to let you talk freely with the people that are important to you.')
+			.addField('Developer', 'This bot is being developed by <@200316185445793792>.', true)
+			.addField('Problem?', 'Did I make a mistake? No worries, just tell <@200316185445793792>.', true)
+			.addField('Open Source', 'You can find the source code at https://github.com/beachdyl/sector/', false)
+			.addField('Privacy', 'In order to make this bot work, I have to store your username and the nickname I assign in the same place. Nobody except the bot developer can find out which real user has which nickname. The server owner can prohibit members from interacting with the bot without ever knowing who they are.', false)
+			.addField('Safety', 'The bot developer can (sometimes) access information about which real person sent what message. If a serious safety concern arises, they could use this information to file reports as appropriate, but it would take a decent amount of work. Requesting a new nickanme will not hide this information.', false)
+			.setThumbnail('https://i.ibb.co/jgNS3rV/2175853.png')
 			.setTimestamp();
 		await interaction.reply({ephemeral: false, embeds: [exampleEmbed] });
 	},
 };
-
